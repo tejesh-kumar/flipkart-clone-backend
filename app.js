5fc05e94422c3a63440d314169590b5cddbd8c3f
tejesh-kumar <tejeshmuralikrishna@gmail.com>
2022-07-16 18:20:01 +0530

    connect to mongodb through mongoose

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const createError = require('http-errors');
+require('dotenv').config();
+
+const { PORT, MONGODB_URI } = process.env;
 
 const app = express();
-const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
 app.use('/', (req, res) => res.send('Flipkart Clone'));
+
+mongoose
+    .connect(MONGODB_URI)
+    .then(() => {
+        console.log('connected to mongodb');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.use((req, res, next) => {
     next(createError(404, 'Not Found'));
