3e31851004712ed893dca2fd532b5ba5e23f4e4f
tejesh-kumar <tejeshmuralikrishna@gmail.com>
2022-08-13 13:25:58 +0530

    setup common connection file to mongodb

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const createError = require('http-errors');
+
 require('dotenv').config();
 
-const { PORT, MONGODB_URI } = process.env;
+const { PORT } = process.env;
+
+const db = require('./db');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/', (req, res) => res.send('Flipkart Clone'));
-
-mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-        console.log('connected to mongodb');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
 
 app.use((req, res, next) => {
     next(createError(404, 'Not Found'));
@@ -34,4 +27,11 @@ app.use((err, req, res) => {
     });
 });
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+// eslint-disable-next-line no-unused-vars
+db.initDb((err, _db) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+    }
+});
